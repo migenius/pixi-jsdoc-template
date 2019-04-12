3579e5ed94de02efeb5989148c5d54b6eea41870
Brendan Hack <brendan@migenius.com>
2019-04-13 07:46:12 +1000

    use application name for index title if available

--- publish.js
+++ publish.js
@@ -513,7 +513,7 @@ exports.publish = function(taffyData, opts, tutorials) {
     var files = find({kind: 'file'}),
         packages = find({kind: 'package'});
 
-    generate('Index',
+    generate(conf.applicationName ? conf.applicationName : 'Index',
         packages.concat(
             [{kind: 'mainpage', readme: opts.readme, longname: (opts.mainpagetitle) ? opts.mainpagetitle : 'Main Page'}]
         ).concat(files),
